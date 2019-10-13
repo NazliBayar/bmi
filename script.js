bc4a6a616bb30c25241ffeaf440d8628bd20a84b
Nazlı Bayar Gönen <nazlibayargonen@Nazls-MacBook-Pro.local>
2019-10-13 23:27:37 +0300

    set up

---
+++ script.js
@@ -0,0 +1,10 @@
+var massJohn = 72;
+var massMark = 89;
+var heightJohn = 1.79;
+var heightMark = 1.85;
+
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+var BMIMark = massMark / (heightMark * heightMark);
+console.log(BMIJohn , BMIMark);
+
+var isJohnBMIBigger = B
